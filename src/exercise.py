d19d2646ff595eecb13ae1a377b40537e78ba654
MrSullivan <>
2021-04-20 22:32:30 +0000

    so much

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,24 @@
 def main():
     #write your code below this line
+    amountOfPositiveNumbers = 0
+    sumOfNumbers = 0
+    
+
+    while True:
+      number = int(input("Give a number:"))
+
+      if (number == 0):
+        break
+      
+      if (number > 0):
+        amountOfPositiveNumbers = amountOfPositiveNumbers + 1
+        sumOfNumbers = sumOfNumbers + number
+        averageOfNumbers = int(sumOfNumbers/amountOfPositiveNumbers)
+    
+    if(averageOfNumbers>0):
+      print(averageOfNumbers)
+    else:
+      print("Cannot calculate the average")
 
 if __name__ == '__main__':
     main()
